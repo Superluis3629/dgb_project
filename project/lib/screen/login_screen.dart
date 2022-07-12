3c36035b449203a3bf98df8d9870fd315d62d827
Superluis3629 <108126524+Superluis3629@users.noreply.github.com>
2022-07-12 14:40:12 +0700

    Update login_screen.dart

--- project/lib/screen/login_screen.dart
+++ project/lib/screen/login_screen.dart
@@ -1,8 +1,14 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, implementation_imports, unnecessary_import
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:project/model/profile.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -12,10 +18,107 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final formkey = GlobalKey<FormState>();
+  Profile profile = Profile(email: '', password: '');
+  final Future<FirebaseApp> firebase = Firebase.initializeApp();
+
+  get style => null;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Hello!!'),
-    );
+    return FutureBuilder(
+        future: firebase,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("Error"),
+              ),
+              body: Center(child: Text("$snapshot.error")),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("ເຂົ້າສູ່ລະບົບ"),
+              ),
+              body: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Form(
+                    key: formkey,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("ອີເມວ",
+                              style: GoogleFonts.notoSansLao(
+                                  textStyle: style, fontSize: 20)),
+                          TextFormField(
+                            validator: MultiValidator([
+                              RequiredValidator(errorText: "ກະລຸນາປ້ອນຂໍ້ມູນ"),
+                              EmailValidator(errorText: "ຮູບແບບອີເມວບໍ່ຖືກຕ້ອງ")
+                            ]),
+                            keyboardType: TextInputType.emailAddress,
+                            onSaved: (email) async {
+                              profile.email = email!;
+                            },
+                          ),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text("ລະຫັດຜ່ານ",
+                              style: GoogleFonts.notoSansLao(
+                                  textStyle: style, fontSize: 20)),
+                          TextFormField(
+                            validator: RequiredValidator(
+                                errorText: "ກະລຸນາປ້ອນລະຫັດຜ່ານ"),
+                            obscureText: true,
+                            onSaved: (password) async {
+                              profile.password = password!;
+                            },
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              child: Text("ເຂົ້າສູ່ລະບົບ",
+                                  style: GoogleFonts.notoSansLao(
+                                      textStyle: style, fontSize: 20)),
+                              onPressed: () async {
+                                if (formkey.currentState!.validate()) {
+                                  formkey.currentState!.save();
+                                  try {
+                                    await FirebaseAuth.instance
+                                        .signInWithEmailAndPassword(
+                                            email: profile.email,
+                                            password: profile.password)
+                                        .then((value) {
+                                      formkey.currentState!.reset();
+                                      Navigator.pushReplacement(context,
+                                          MaterialPageRoute(builder: (context) {
+                                        return LoginScreen();
+                                      }));
+                                    });
+                                  } on FirebaseAuthException catch (e) {
+                                    Fluttertoast.showToast(
+                                        msg: e.message!,
+                                        gravity: ToastGravity.CENTER);
+                                  }
+                                }
+                              },
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }
+          return Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        });
   }
 }
